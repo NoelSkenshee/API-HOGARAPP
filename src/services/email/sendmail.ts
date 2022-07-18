ac1dd9b9ad88881f732e8f1802da4640e2ba5702
NoelSkenshee <augustinpreacher@gmail.com>
2022-07-18 11:02:52 -0400

    Change sendgrid for @sendgrid/mail

--- src/services/email/sendmail.ts
+++ src/services/email/sendmail.ts
@@ -1,4 +1,4 @@
-import sg from "sendgrid"; //(process.env.SENDGRID_API_KEY);
+import sg from "@sendgrid/mail"; //(process.env.SENDGRID_API_KEY);
 
 import Utils from "../Utils";
 
@@ -8,33 +8,16 @@ export default class Mail extends Utils {
     super();
   }
   public static verify_user_mail(name: string, to: string, token: string) {
-    const mail = sg(Mail.senKey);
+     sg.setApiKey(this.senKey)
     const payload = Utils.getMialPayload();
-    var request = mail.emptyRequest({
-      method: "POST",
-      path: "/v3/mail/send",
-      body: {
-        personalizations: [
-          {
-            to: [
-              {
-                email: to,
-              },
-            ],
-            subject: payload.subject(),
-          },
-        ],
-        from: {
-          email: process.env.HOGARAPP_EMAIL,
-        },
-        content: [
-          {
-            type: "text/html",
-            value: payload.text(name, token),
-          },
-        ],
-      },
-    });
-    return mail.API(request);
-  }
+     const msg={
+      from: process.env.HOGARAPP_EMAIL?process.env.HOGARAPP_EMAIL:"",
+      to,
+      subject: payload.subject(),
+      text:"",
+      html:payload.text(name, token)
+     };
+  return   sg.send(msg)
+   
+}
 }
